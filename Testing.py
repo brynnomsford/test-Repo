d1394b9f15a74dde2d073c250dcb1dce5053725c
Sarah Sirignano <brynnomsford@yahoo.com>
2024-09-15 10:27:54 -0400

    Is this the commit message?

---
+++ Testing.py
@@ -0,0 +1,16 @@
+import math
+import os
+import sys
+import requests
+
+# print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+r = requests.get("https://hwpbc.org")
+print(r.status_code)
